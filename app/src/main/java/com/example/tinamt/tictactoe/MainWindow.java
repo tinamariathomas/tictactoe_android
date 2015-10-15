7a83b3193872c8cb96296d1661137a36d8a1313a
tinamaria <tinamariathomas@gmail.com>
2015-10-15 13:20:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tinamt/tictactoe/MainWindow.java
@@ -0,0 +1,54 @@
+package com.example.tinamt.tictactoe;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+
+
+public class MainWindow extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_window);
+
+        Button buttons[][] = new Button[3][3];
+        for (int i = 0; i < 3; i++) {
+            TableRow tr = new TableRow(this);
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j] = new Button(this);
+                buttons[i][j].setText("-");
+                buttons[i][j].setWidth(10);
+                buttons[i][j].setHeight(10);
+
+
+            }
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_window, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
